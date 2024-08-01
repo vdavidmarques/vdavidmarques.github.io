80f43cfffe1ff8b76026389ca484feeaaebe4b20
vdavidmarques <vdavidmarques@gmail.com>
2024-08-01 14:33:53 -0300

    chore: Updating cloudaitech

--- portfolio/php/cloudaitech/components/main-text.php
+++
@@ -1,6 +0,0 @@
-<article class="main-text container min-h-[43.4375rem] lg:min-h-[53.0625rem] flex flex-col items-center justify-center text-center">
-    <h2 class="heading-title-2xl mb-5">Blockchain Beyond Cryptocurrency: Transforming Industries</h2>
-    <p class="heading-text-xl">
-        Discover how blockchain technology extends far beyond cryptocurrencies, reshaping industries like finance, supply chain management, and voting systems, with its decentralized and transparent nature.
-    </p> 
-</article> 
--- portfolio/php/cloudaitech/index.php
+++
@@ -1,8 +0,0 @@
-<?php include 'header.php' ?>
-
-<?php include 'components/main-text.php' ?>
-<?php include 'components/text-image.php' ?>
-<?php include 'components/image-text.php' ?>
-<?php include 'components/depositions.php' ?>
-
-<?php include 'footer.php' ?>
